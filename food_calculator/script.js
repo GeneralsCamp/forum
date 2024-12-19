7d3703b3e992c37778654fb41ae86fe32b833253
GeneralsCamp <generalscampofficial@gmail.com>
2024-12-19 23:31:30 +0100

    Update script.js

--- food_calculator/script.js
+++ food_calculator/script.js
@@ -50,7 +50,7 @@ function LocationModify() {
 
 function Calculate() {
     CalculateBonuses();
-    const buildings = ["b1", "b2", "b3", "b4", "b5"];
+    const buildings = ["b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8"];
     buildings.forEach(updateBuilding);
 }
 
