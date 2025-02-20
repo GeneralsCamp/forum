bc87703568c6043e933a3a44caef0ad3d8fe6b90
GeneralsCamp <generalscampofficial@gmail.com>
2025-02-20 10:16:54 +0100

    Add files via upload

--- food_calculator/script.js
+++ food_calculator/script.js
@@ -89,12 +89,16 @@ function CalculateBonuses() {
     deco2 = 0;
     deco2 = document.getElementById("deco2").value / 100;
 
-    if (selectBeri.value == "kingdoms") {
-        beriKingdoms = 0.20;
-        beriGreen = 0;
+    if (selectBeri.value == "weaponmaster") {
+        beriKingdoms = 0.40;
+        beriGreen = 0.20;
     }
-    else if (selectBeri.value == "both") {
+    else if (selectBeri.value == "deathclaw") {
         beriKingdoms = 0.20;
+        beriGreen = 0.20;
+    }
+    else if (selectBeri.value == "widowmaker") {
+        beriKingdoms = 0;
         beriGreen = 0.20;
     }
     else {
