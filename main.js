c827f433d4ba2246f65733b1902a2bad87f15771
Dániel <generalscampofficial@gmail.com>
2026-01-10 20:26:43 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const categories = {
         { name: "Look-items Overview", desc: "This page loads the in-game look items automatically!", icon: "look-items.webp", link: "./look_items/index.html", disabled: false },
         { name: "Gacha Overview", desc: "This page loads the in-game gacha rewards automatically!", icon: "gacha-icon.webp", link: "./gacha_overview/index.html", disabled: false },
         { name: "Event plan Overview", desc: "This page loads the event plans automatically!", icon: "event-plan-icon.webp", link: "./eventplan/index.html", disabled: false },
-        { name: "Generals Overview", desc: "This page loads the in-game generals automatically!", icon: "generals-icon.webp", link: "#", disabled: true },
+        { name: "Generals Overview", desc: "This page loads the in-game generals automatically!", icon: "generals-icon.webp", link: "#", disabled: false },
     ],
 
     calculators: [
