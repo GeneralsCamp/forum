674639543915d663ebd2c7cb775ab31a442d82b3
Balogh Dániel <diak_190.balogh.daniel@11.b.2026.com>
2026-01-31 19:04:31 +0100

    DELETE ALL OLD FILES

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
 const categories = {
     overviews: [
-        { name: "Decorations Overview", desc: "This page loads the in-game decorations automatically!", icon: "decorations.webp", link: "./decorations/index.html", disabled: false },
-        { name: "CI's & TCI's Overview", desc: "This page loads the in-game CI's & TCI's automatically!", icon: "ci-icon.webp", link: "./building_items/index.html", disabled: false },
-        { name: "Look-items Overview", desc: "This page loads the in-game look items automatically!", icon: "look-items.webp", link: "./look_items/index.html", disabled: false },
-        { name: "Gacha Overview", desc: "This page loads the in-game gacha rewards automatically!", icon: "gacha-icon.webp", link: "./gacha_overview/index.html", disabled: false },
-        { name: "Generals Overview", desc: "This page loads the in-game generals automatically!", icon: "generals-icon.webp", link: "./generals_overview/index.html", disabled: false },
-        { name: "Event plan Overview", desc: "This page loads the event plans automatically!", icon: "event-plan-icon.webp", link: "./eventplan/index.html", disabled: false },
-        { name: "Lootbox Overview", desc: "This page loads the in-game lootboxes key chances automatically!", icon: "lootbox-icon.webp", link: "./loot_box/index.html", disabled: false }
+        { name: "Decorations Overview", desc: "This page loads the in-game decorations automatically!", icon: "decorations.webp", link: "./overviews/decorations/index.html", disabled: false },
+        { name: "CI's & TCI's Overview", desc: "This page loads the in-game CI's & TCI's automatically!", icon: "ci-icon.webp", link: "./overviews/building_items/index.html", disabled: false },
+        { name: "Look-items Overview", desc: "This page loads the in-game look items automatically!", icon: "look-items.webp", link: "./overviews/look_items/index.html", disabled: false },
+        { name: "Gacha Overview", desc: "This page loads the in-game gacha rewards automatically!", icon: "gacha-icon.webp", link: "./overviews/gacha_overview/index.html", disabled: false },
+        { name: "Generals Overview", desc: "This page loads the in-game generals automatically!", icon: "generals-icon.webp", link: "./overviews/generals_overview/index.html", disabled: false },
+        { name: "Lootbox Overview", desc: "This page loads the in-game lootboxes key chances automatically!", icon: "lootbox-icon.webp", link: "./overviews/loot_box/index.html", disabled: false },
+        { name: "Event plan Overview", desc: "This page loads the event plans automatically!", icon: "event-plan-icon.webp", link: "./overviews/event_plan/index.html", disabled: false },
     ],
 
     calculators: [
-        { name: "Food Production Calculator", desc: "Calculate your in-game food production easily.", icon: "food-production-icon.webp", link: "./food_calculator/index.html", disabled: false },
-        { name: "Mead Production Calculator", desc: "Calculate your mead production easily.", icon: "mead-icon.webp", link: "./brewery_simulator/index.html", disabled: false },
-        { name: "Wall Limit Calculator", desc: "Calculate your wall unit limit with absolute accuracy.", icon: "wall-icon.webp", link: "./wall_limit/index.html", disabled: false },
-        { name: "Collector Event Calculator", desc: "Calculate that how many points you need to reach your goal.", icon: "collector-icon.webp", link: "./collector_calculator/index.html", disabled: false },
-        { name: "Detection Time Calculator", desc: "Calculate the exact land time, and detection time of an attack.", icon: "detection-icon.webp", link: "./travel_speed/index.html", disabled: false },
-        { name: "Rift Raid Point Calculator", desc: "Calculate Activity Points based on defeated/faced troops, area, and boss level.", icon: "rift-raid-points-icon.webp", link: "./rift_raid_points/index.html", disabled: false }
+        { name: "Food Production Calculator", desc: "Calculate your in-game food production easily.", icon: "food-production-icon.webp", link: "./calculators/food_production/index.html", disabled: false },
+        { name: "Mead Production Calculator", desc: "Calculate your mead production easily.", icon: "mead-icon.webp", link: "./calculators/mead_production/index.html", disabled: false },
+        { name: "Wall Limit Calculator", desc: "Calculate your wall unit limit with absolute accuracy.", icon: "wall-icon.webp", link: "./calculators/wall_limit/index.html", disabled: false },
+        { name: "Collector Event Calculator", desc: "Calculate that how many points you need to reach your goal.", icon: "collector-icon.webp", link: "./calculators/collector_event/index.html", disabled: false },
+        { name: "Detection Time Calculator", desc: "Calculate the exact land time, and detection time of an attack.", icon: "detection-icon.webp", link: "./calculators/travel_speed/index.html", disabled: false },
+        { name: "Rift Raid Point Calculator", desc: "Calculate Activity Points based on defeated/faced troops, area, and boss level.", icon: "rift-raid-points-icon.webp", link: "./calculators/rift_raid_points/index.html", disabled: false }
     ],
 
     simulators: [
-        { name: "Castle Layout Simulator", desc: "Design and manage your layouts manually or automatically.", icon: "layout-icon.webp", link: "./layout_editor/index.html", disabled: false },
-        { name: "Hall of Legends Simulator", desc: "Try point allocations without spending rubies.", icon: "hall-of-legends-simulator-icon.webp", link: "./hol_simulator/index.html", disabled: false },
-        //{ name: "Imperial Patronage Simulator", desc: "Try to upgrade the decorations without any in-game resources.", icon: "patronage-icon.webp", link: "#", disabled: true },
-        //{ name: "Battle Simulator", desc: "Try to attack or defend without any in-game losses.", icon: "battle-simulator-icon.webp", link: "#", disabled: true }
+        { name: "Castle Layout Simulator", desc: "Design and manage your layouts manually or automatically.", icon: "layout-icon.webp", link: "./simulators/layout_editor/index.html", disabled: false },
+        { name: "Hall of Legends Simulator", desc: "Try point allocations without spending rubies.", icon: "hall-of-legends-simulator-icon.webp", link: "./simulators/hol_simulator/index.html", disabled: false },
     ]
 };
 
