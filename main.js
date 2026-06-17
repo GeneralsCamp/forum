d471ee434ae3c7c7c5770713cb23c1048d9c7d45
Balogh Dániel <diak_190.balogh.daniel@11.b.2026.com>
2026-06-17 14:00:55 +0200

    Small changes

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ const categories = {
         { name: "Imperial Patronage Simulator", desc: "Simulate donations, thresholds, and rewards.", icon: "patronage-icon.webp", link: "./simulators/imperial_patronage/index.html", disabled: false },
         { name: "Castle Layout Simulator", desc: "Design and manage your castle layouts.", icon: "main_page/layout-icon.webp", link: "./simulators/layout_editor/index.html", disabled: false },
         { name: "Hall of Legends Simulator", desc: "Try point allocations without spending rubies.", icon: "main_page/hall-of-legends-simulator-icon.webp", link: "./simulators/hol_simulator/index.html", disabled: false },
-        { name: "Equipment Builder", desc: "Build custom commander and castellan equipment sets.", icon: "main_page/equipment-icon.webp", link: "./simulators/equipment_builder/index.html", disabled: true },
+        { name: "Equipment Builder", desc: "Build custom commander and castellan equipment sets.", icon: "main_page/equipment-icon.webp", link: "./simulators/equipment_builder/index.html", disabled: false },
     ]
 };
 
